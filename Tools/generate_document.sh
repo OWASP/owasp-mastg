6a0d80885078cc5cdd9cd8f74622f50abc843857
Bernhard Mueller <bernhard.mueller11@gmail.com>
2017-01-21 09:33:59 +0800

    Update generate_document.sh

--- Tools/generate_document.sh
+++ Tools/generate_document.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 type pandoc >/dev/null 2>&1 || { echo >&2 "I require pandoc but it's not installed.  Aborting."; exit 1; }
 # How to generate Docx
-cd Document
-pandoc -f markdown_github --columns 10000 -t docx -o ../MSTG.docx *.md Testcases/*.md
-pandoc -f markdown_github --columns 10000 -t html -o ../MSTG.html *.md Testcases/*.md
+cd ../Document
+pandoc -f markdown_github --columns 10000 -t docx -o ../Generated/MSTG.docx *.md Testcases/*.md
+pandoc -f markdown_github --columns 10000 -t html -o ../Generated/MSTG.html *.md Testcases/*.md
