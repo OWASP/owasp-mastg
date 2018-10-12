c5fa176a3dbcf99928be8eabaef460db043bbc81
Jeroen Willemsen <jeroenwillemsen2001@gmail.com>
2018-10-12 13:00:04 -0700

    #1002: added necessary image and updated running.js (WIP)

--- Tools/running.js
+++ Tools/running.js
@@ -1,9 +1,14 @@
-exports.header = null
+exports.header = {
+  height: "1cm"
+  contents: function(pageNum, numPages){
+    return "<span style='float:right;'><img src='Images/OWASP_logo-bw.png'></span>"
+  }
+}
 
 
 exports.footer = {
   height: "1cm",
   contents: function(pageNum, numPages) {
-    return "<span style='float:left;font-size:10px;'>[DATE]</span> <span style='float:right;font-size:10px;'>" + pageNum + " / " + numPages + "</span>"
+    return "<span style='float:left;font-size:10px;'>[DATE]</span> <span style='float:right;font-size:10px;'>" + pageNum + "</span>"
   }
 }
