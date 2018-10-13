e0160a24aea231fd01069eb69de05a8e2a220f28
Jeroen Willemsen <jeroenwillemsen2001@gmail.com>
2018-10-13 11:15:56 -0700

    #1009: wip

--- Tools/updateLeanpub.sh
+++ Tools/updateLeanpub.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 echo "Before you can run it, don't forget to update your apikey in ~/.leanpub.json. See https://leanpub.com/author_dashboard/settings for the api key."
+npm install -g leanpub-cli
