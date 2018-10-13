7d8288716c7cc0def07bf53f760a3aba32065091
Jeroen Willemsen <jeroenwillemsen2001@gmail.com>
2018-10-13 07:51:32 -0700

    #1002: small udpates

--- Tools/gitbookepubandpdf.sh
+++ Tools/gitbookepubandpdf.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+echo "Usage: ./gitbookepubandpdf VERSIONTAG"
 echo "Do not forget to install npm, gitbookcli (https://www.npmjs.com/package/gitbook-cli), calibre (brew cask install calibre)"
+echo 'Versiontag is' $1
+cp book.json ../book.json
+sed -i.bak "s/\[\]/$1/g" ../book.json
+rm ../book.json.bak
 gitbook install ../
 
 gitbook pdf ../ ../Generated/MSTG-eng.pdf
