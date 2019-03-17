dd52eac0ecf62f8fc347b48c2ce7141c752761b3
Jeroen Willemsen <jwillemsen@xebia.com>
2019-03-17 21:46:11 +0100

    #1156: first attempt to fix changelog typedifference for book/ebook

--- Tools/gitbookepubandpdf.sh
+++ Tools/gitbookepubandpdf.sh
@@ -3,6 +3,8 @@ echo "Usage: ./gitbookepubandpdf VERSIONTAG"
 echo "Do not forget to install npm, gitbookcli (https://www.npmjs.com/package/gitbook-cli), calibre (brew cask install calibre on Mac OS X)"
 echo 'Versiontag is' $1
 cp book.json ../book.json
+cp ../CHANGELOG.MD ../Document/Changelog.md
+sed  "s/\.\.\/Changelog\.md/Changelog\.md/g" ../Document/SUMMARY.md
 sed -i.bak "s/\[\]/$1/g" ../book.json
 rm ../book.json.bak
 gitbook install ../
@@ -10,5 +12,6 @@ gitbook install ../
 gitbook pdf ../ ../Generated/MSTG-EN.pdf
 gitbook epub ../ ../Generated/MSTG-EN.epub
 gitbook mobi ../ ../Generated/MSTG-EN.mobi
-
+sed  "s/Changelog\.md/\.\.\/Changelog\.md/g" ../Document/SUMMARY.md
+rm ../Document/Changelog.md
 echo "We are done: please do not forget to update the leanpub update!"
