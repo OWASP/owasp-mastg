38f27386054e5ee8a56281c6284fb5e055cea651
Jeroen Willemsen <jwillemsen@xebia.com>
2019-05-14 21:15:30 +0200

    #1204: made the generation of documents shorter as well

--- Tools/gendocs.sh
+++ Tools/gendocs.sh
@@ -4,5 +4,10 @@ echo "Applying Link check"
 sh ./Apply_Link_Check.sh
 echo "Applying Linter check"
 sh ./Apply_Linter_Check.sh
+
+if [ -z "$TRAVIS_TAG" ]; then 
+exit 0; 
+fi
+echo "Running creaton of pdfs and word documents"
 sh ./gitbookepubandpdf.sh $TRAVIS_TAG
 sh ./generate_document.sh
