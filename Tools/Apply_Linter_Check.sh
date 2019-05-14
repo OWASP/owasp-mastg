e4e6aeda61e2dc0254eb8bc14443a8558df25cc3
Jeroen Beckers <info@dauntless.be>
2019-05-14 20:10:49 +0200

    Add linting and link checking during build

---
+++ Tools/Apply_Linter_Check.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Script taken from https://github.com/OWASP/CheatSheetSeries/blob/master/scripts/Apply_Linter_Check.sh
+# Script in charge of auditing the released MD files with the linter policy defined at project level
+cd ..
+markdownlint -c .markdownlint.json -o linter-result.out Document
+errors=`wc -m linter-result.out | cut -d' ' -f1`
+content=`cat linter-result.out`
+if [[ $errors != "0" ]]
+then
+    echo "[!] Error(s) found by the Linter: $content"
+    echo "Only warning for now..."
+    #exit $errors
+else
+    echo "[+] No error found by the Linter."
+fi
--- Tools/gendocs.sh
+++ Tools/gendocs.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 cd $TRAVIS_BUILD_DIR/Tools
+echo "Applying Link check"
+sh ./Apply_Link_Check.sh
+echo "Applying Linter check"
+sh ./Apply_Linter_Check.sh
 sh ./gitbookepubandpdf.sh $TRAVIS_TAG
 sh ./generate_document.sh
