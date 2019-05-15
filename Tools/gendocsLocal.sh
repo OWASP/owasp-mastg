e023fa8196395593a4767102c52d044579e10c8f
Jeroen Willemsen <jwillemsen@xebia.com>
2019-05-15 22:17:33 +0200

    #1204: migration preparation for autoreleaser

--- Tools/gendocsLocal.sh
+++ Tools/gendocsLocal.sh
@@ -5,13 +5,8 @@ sh ./Apply_Linter_Check.sh
 echo "Counting amount of linter issues:"
 export RESULT=$(wc -l ../linter-result.out)
 echo $RESULT
-if [ "$TRAVIS_PULL_REQUEST" != "false" ] ; then
-    echo "Applying Link check"
-    sh ./Apply_Link_Check.sh
-fi
-if [ -z "$TRAVIS_TAG" ]; then 
-exit 0; 
-fi
+echo "Applying Link check"
+sh ./Apply_Link_Check.sh
 echo "Running creaton of pdfs and word documents"
 sh ./gitbookepubandpdf.sh $TRAVIS_TAG
 sh ./generate_document.sh
--- Tools/report_to_github.sh
+++ Tools/report_to_github.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
-echo "Testing result"
-echo $RESULT
+if [ "$TRAVIS_PULL_REQUEST" != "false" ] ; then
+   echo "Testing result"
+   echo $RESULT
+#    curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST \
+#     -d "{\"body\": \"Hello world $RESULT\"}" \
+#     "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
+fi
