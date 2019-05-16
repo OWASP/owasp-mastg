ae60e58bfac6678b45ae0db5edb6cc15c7575137
Jeroen Willemsen <jwillemsen@xebia.com>
2019-05-16 08:41:59 +0200

    #1204: fixed booboe

--- Tools/gendocs.sh
+++ Tools/gendocs.sh
@@ -7,11 +7,11 @@ LINTRESULT=$(wc -l ../linter-result.out)
 echo $RESULT
 if [ "$TRAVIS_PULL_REQUEST" != "false" ] ; then
     echo "Applying Link check"
-    export LINKRESULT:$(sh ./Apply_Link_Check.sh)
+    export LINKRESULT=$(sh ./Apply_Link_Check.sh)
     echo "$LINKRESULT"
     echo "Sending feedback to Github"
     curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST \
-    -d "{\"body\": \"Broken link result: $LINKRESULT, markdown result errorlength: $LINTRESULT. If fields are empty: you are OK. If fields contain data: please check for markdown errors. \"}" \
+    -d "{\"body\": \"Broken link result: $LINKRESULT, markdown result errorlength: $LINTRESULT. If fields are empty or 0: you should be OK. If fields contain data: please check for markdown and link errors. \"}" \
     "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
 fi
 if [ -z "$TRAVIS_TAG" ]; then 
