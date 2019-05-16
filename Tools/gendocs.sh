0dd774a9c64fa448fdf356821129660ab83b25e3
Jeroen Willemsen <jwillemsen@xebia.com>
2019-05-16 09:48:31 +0200

    #1204: final update for feedback message

--- Tools/gendocs.sh
+++ Tools/gendocs.sh
@@ -11,7 +11,7 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" ] ; then
     echo "$LINKRESULT"
     echo "Sending feedback to Github"
     curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST \
-    -d "{\"body\": \"Broken link result: $LINKRESULT, markdown result errorlength: $LINTRESULT. If fields are empty or 0: you should be OK. If fields contain data: please check for markdown and link errors. \"}" \
+    -d "{\"body\": \"Results for commit $TRAVIS_COMMIT: Broken link result: $LINKRESULT, markdown result errorlength: $LINTRESULT. If fields are empty or 0: you should be OK. If fields contain data: please check for markdown and link errors. \"}" \
     "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
 fi
 if [ -z "$TRAVIS_TAG" ]; then 
