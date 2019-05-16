d97daaa2facfe81bbd8f1761ac1c89b37ea201ce
Jeroen Willemsen <jwillemsen@xebia.com>
2019-05-16 08:27:54 +0200

    #1204: Tools updated

--- Tools/gendocs.sh
+++ Tools/gendocs.sh
@@ -8,8 +8,10 @@ echo $RESULT
 if [ "$TRAVIS_PULL_REQUEST" != "false" ] ; then
     echo "Applying Link check"
     LINKRESULT:$(sh ./Apply_Link_Check.sh)
+    echo "$LINKRESULT"
+    echo "Sending feedback to Github"
     curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST \
-    -d "{\"body\": \"Broken link result: $LINKRESULT, markdown result: $LINTRESULT\"}" \
+    -d "{\"body\": \"Broken link result: $LINKRESULT, markdown result errorlength: $LINTRESULT\"}" \
     "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
 fi
 if [ -z "$TRAVIS_TAG" ]; then 
