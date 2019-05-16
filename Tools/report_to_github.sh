37a23247d94a8af8a82274652397627dd0685301
Jeroen Willemsen <jwillemsen@xebia.com>
2019-05-16 07:09:57 +0200

    #1204: first feedbackemssage

--- Tools/report_to_github.sh
+++ Tools/report_to_github.sh
@@ -2,7 +2,7 @@
 if [ "$TRAVIS_PULL_REQUEST" != "false" ] ; then
    echo "Testing result"
    echo $RESULT
-#    curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST \
-#     -d "{\"body\": \"Hello world $RESULT\"}" \
-#     "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
+   curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST \
+    -d "{\"body\": \"Hello world $RESULT\"}" \
+    "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
 fi
