e69a9ef46a755d6e66bd591707cc616a82c727cd
Jeroen Willemsen <jeroenwillemsen2001@gmail.com>
2019-06-02 07:33:28 +0200

    less_noise: added better instructions

--- Tools/gendocs.sh
+++ Tools/gendocs.sh
@@ -9,9 +9,21 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" ] ; then
     echo "Applying Link check"
     export LINKRESULT=$(sh ./Apply_Link_Check.sh)
     echo "$LINKRESULT"
+    if [ "$LINTRESULT" -eq "0" ]; then
+        COMMENT_LINT="Please run the Apply_Linter_check.sh in the tools folder to see where the Lint issues are."
+    fi
+    if [ "$LINTRESULT" -eq "" ]; then
+        COMMENT_LINT="Please run the Apply_Linter_check.sh in the tools folder to see where the Lint issues are."
+    fi
+    if [ "$LINKRESULT" -eq "0" ]; then
+        COMMENT_LINK="Please run the Apply_Link_Check.sh in the tools folder to see wherhe the Link issues are."
+    fi
+    if [ "$LINKRESULT" -eq "" ]; then
+        COMMENT_LINK="Please run the Apply_Link_Check.sh in the tools folder to see wherhe the Link issues are."
+    fi
     echo "Sending feedback to Github"
     curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST \
-    -d "{\"body\": \"Results for commit $TRAVIS_COMMIT: \n Broken link result: $LINKRESULT, \n markdown result errorlength: $LINTRESULT. \n If fields are empty or 0: you should be OK. If fields contain data: please check for markdown and link errors. \"}" \
+    -d "{\"body\": \"Results for commit $TRAVIS_COMMIT: Broken link result: $LINKRESULT .\n $COMMENT_LINK \n Markdown result errorlength: $LINTRESULT .\n $COMMENT_LINT  \"}" \
     "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
 fi
 if [ -z "$TRAVIS_TAG" ]; then 
