51b9c3208474ae90a122d2cb0a16a1de5d90abac
Jeroen Willemsen <jwillemsen@xebia.com>
2019-08-03 11:50:15 +0200

    epub example

--- Tools/gitbookepubandpdf.sh
+++ Tools/gitbookepubandpdf.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
-echo "Usage: ./gitbookepubandpdf VERSIONTAG"
-echo "Do not forget to install npm, gitbookcli (https://www.npmjs.com/package/gitbook-cli), calibre (brew cask install calibre on Mac OS X)"
-echo 'Versiontag is' $1
-cp book.json ../book.json
-sed -i.bak "s/\[\]/$1/g" ../book.json
-rm ../book.json.bak
-gitbook install ../
+# echo "Usage: ./gitbookepubandpdf VERSIONTAG"
+# echo "Do not forget to install npm, gitbookcli (https://www.npmjs.com/package/gitbook-cli), calibre (brew cask install calibre on Mac OS X)"
+# echo 'Versiontag is' $1
+# cp book.json ../book.json
+# sed -i.bak "s/\[\]/$1/g" ../book.json
+# rm ../book.json.bak
+# gitbook install ../
 
-gitbook pdf ../ ../Generated/MSTG-EN.pdf
+#gitbook pdf ../ ../Generated/MSTG-EN.pdf
+sed -i -.bak 's/^(?!(http)$)md\#/html\#/g' ../Document/*
+rm ../Document/*.bak
 gitbook epub ../ ../Generated/MSTG-EN.epub
-gitbook mobi ../ ../Generated/MSTG-EN.mobi
+sed -i -.bak 's/^(?!(http)$)html\#/md\#/g' ../Document/*
+rm ../Document/*.bak
+#gitbook mobi ../ ../Generated/MSTG-EN.mobi
 
 echo "We are done: please do not forget to update the leanpub update!"
