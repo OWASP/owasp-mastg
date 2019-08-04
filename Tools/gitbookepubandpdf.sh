4a3a7966411433d27a49d5ed50a16951fca0797f
Jeroen Willemsen <jwillemsen@xebia.com>
2019-08-04 08:04:28 +0200

    Release 1.1.3: fixed generation scripts

--- Tools/gitbookepubandpdf.sh
+++ Tools/gitbookepubandpdf.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-# echo "Usage: ./gitbookepubandpdf VERSIONTAG"
-# echo "Do not forget to install npm, gitbookcli (https://www.npmjs.com/package/gitbook-cli), calibre (brew cask install calibre on Mac OS X)"
-# echo 'Versiontag is' $1
-# cp book.json ../book.json
-# sed -i.bak "s/\[\]/$1/g" ../book.json
-# rm ../book.json.bak
-# gitbook install ../
-
+echo "Usage: ./gitbookepubandpdf VERSIONTAG"
+echo "Do not forget to install npm, gitbookcli (https://www.npmjs.com/package/gitbook-cli), calibre (brew cask install calibre on Mac OS X)"
+echo 'Versiontag is' $1
+cp book.json ../book.json
+sed -i.bak "s/\[\]/$1/g" ../book.json
+rm ../book.json.bak
+gitbook install ../
 gitbook pdf ../ ../Generated/MSTG-EN.pdf
 gitbook mobi ../ ../Generated/MSTG-EN.mobi
 gsed -i.bak '/http/b; s/\.md#/.html#/' ../Document/*
