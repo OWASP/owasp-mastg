6b7c291a9b78b61eacc07b2a74f3993095fc713a
Jeroen Willemsen <jwillemsen@xebia.com>
2019-08-04 07:36:10 +0200

    epub_fixes: found a fix

--- Tools/gitbookepubandpdf.sh
+++ Tools/gitbookepubandpdf.sh
@@ -8,10 +8,10 @@
 # gitbook install ../
 
 #gitbook pdf ../ ../Generated/MSTG-EN.pdf
-sed -i -.bak 's/(^((?!http).)+(\.md\#))/html\#/g' ../Document/*
+gsed -i.bak '/http/b; s/\.md#/.html#/' ../Document/*
 rm ../Document/*.bak
 gitbook epub ../ ../Generated/MSTG-EN.epub
-sed -i -.bak 's/(^((?!http).)+(\.html\#))/md\#/g' ../Document/*
+gsed -i.bak '/http/b; s/\.html#/.md#/' ../Document/*
 rm ../Document/*.bak
 #gitbook mobi ../ ../Generated/MSTG-EN.mobi
 
