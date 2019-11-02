27fc229946d865b722e7fd9d2f7045a65e9c118d
cpholguera <perezholguera@gmail.com>
2019-11-02 15:08:40 +0100

    folder and contents removed

--- Crackmes/Write-ups/OMTG-Android-App/02_hook_cipher_init.js
+++
@@ -1,9 +0,0 @@
-Java.perform(function(){
-  console.log("[*] script loaded");
-
-  var Cipher = Java.use("javax.crypto.Cipher");
-  Cipher.init.overload('int', 'java.security.Key').implementation  = function(opmode, key){
-      console.log("[*] Cipher.init called");
-      this.init.overload('int', 'java.security.Key').call(this, opmode, key);
-  }
-});
