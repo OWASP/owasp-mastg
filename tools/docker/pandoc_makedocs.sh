219e4a2cdf93737c4868268f7c130fffb302c5df
cpholguera <perezholguera@gmail.com>
2021-12-14 13:37:20 +0100

    revert pandoc comments

--- tools/docker/pandoc_makedocs.sh
+++ tools/docker/pandoc_makedocs.sh
@@ -20,14 +20,12 @@ PANDOC_PARAMS+="--metadata version=${VERSION} "
 
 PANDOCKER="docker run --rm --volume `pwd`:/pandoc ${IMG}:${TAG} ${PANDOC_PARAMS}"
 
-# docker run --rm --entrypoint '/bin/sh' --volume `pwd`:/pandoc ${IMG}:${TAG} -c 'sed -i "s#<!-- \(.*\) -->#\1#g" Document/*.md'
-
 # remove the HTML comment from \pagebreak
-# if [[ "$OSTYPE" == "darwin"* ]]; then
-#   gsed -i 's#<!-- \(.*\) -->#\1#g' Document/*.md
-# else
-#   sed -i 's#<!-- \(.*\) -->#\1#g' Document/*.md
-# fi
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  gsed -i 's#<!-- \(.*\) -->#\1#g' Document/*.md
+else
+  sed -i 's#<!-- \(.*\) -->#\1#g' Document/*.md
+fi
 
 # Use pandocker PANDOCKER by default, unless `export PANDOC=pandoc`
 # this is useful for CI, because we can run the script directly inside the container
