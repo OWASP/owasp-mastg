a4c1ff1391bfa45b78db5473d1df4a1ace6651f5
cpholguera <perezholguera@gmail.com>
2022-01-19 22:41:57 +0100

    Generate MSTG Checklists automatically and machine-readable YAML (#2010)
    * port masvs checklist generation to the mstg
    * add recursive ls
    * fix Tools -> tools
    * generate MSTG html
    * checkout latest masvs tag
    * use GITHUB_ENV instead of steps.output
    * add MASVS and MSTG link including versions and commit IDs
    * add new logo
    * set avenir as main font
    * add column status with validation
    * add conditional formatting for pass, fail and n/a
    * add step Show openpyxl Version
    * try format only relevant status cells
    * create new About sheet with the same header
    * add intro to project
    * black and flake8 fixes

---
+++ tools/scripts/gen_all_excel.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo $PWD
+LanguageArray=("de" "en" "es" "fa" "fr" "hi" "ja" "ko" "ru" "zhcn" "zhtw")
+for lang in ${LanguageArray[*]}; do
+    cd owasp-masvs/tools && python3 ./export.py -f yaml -l $lang > masvs_$lang.yaml && cd -
+    python3 parse_html.py -m owasp-masvs/tools/masvs_$lang.yaml -i generated/html -o masvs_full_$lang.yaml
+    python3 yaml_to_excel.py -m masvs_full_$lang.yaml -o Mobile_App_Security_Checklist_$lang.xlsx --mstgversion $1 --mstgcommit $2 --masvsversion $3 --masvscommit $4
+done
