ae25710482ed6708b172edb8696bff375446b687
Carlos Holguera <perezholguera@gmail.com>
2024-01-15 23:15:03 +0100

    remove mstg- rules

--- risks/MASVS-PLATFORM/1-secure-ipc/unauthenticated-ipc/android-clipboard/mstg-storage-6.2.java
+++
@@ -1,9 +0,0 @@
-public class MainActivity extends AppCompatActivity {
-    private void test(){
-        ClipboardManager clipboard = (ClipboardManager)getSystemService(Context.CLIPBOARD_SERVICE);
-
-        ClipData clip = ClipData.newPlainText("simple secret", "Hello, World!");
-        // ruleid: MSTG-STORAGE-6.2
-        clipboard.setPrimaryClip(clip);
-    }
-}
