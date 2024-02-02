374b9ab65e5107153347c8a567fd65b19a5ef097
Carlos Holguera <perezholguera@gmail.com>
2024-02-02 12:12:40 +0100

    rm -q option

--- risks/MASVS-CRYPTO/1-strong-crypto/insecure-random/android-non-random-use/example-1/run.sh
+++ risks/MASVS-CRYPTO/1-strong-crypto/insecure-random/android-non-random-use/example-1/run.sh
@@ -1,2 +1,2 @@
-NO_COLOR=true semgrep -c ../rules/mastg-android-non-random-use.yaml ./non-random.java --text -q -o output.txt
+NO_COLOR=true semgrep -c ../rules/mastg-android-non-random-use.yaml ./non-random.java --text -o output.txt
 NO_COLOR=true semgrep -c ../rules/mastg-android-non-random-use.yaml ./non-random.java --sarif -o output.sarif
