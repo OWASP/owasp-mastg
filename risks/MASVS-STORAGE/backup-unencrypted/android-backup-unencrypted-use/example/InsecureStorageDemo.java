5ff34f907820af7a8cc970fcc1315db374d525b6
E.A <46949964+e-a-security@users.noreply.github.com>
2024-04-14 15:30:28 -0500

    Update InsecureStorageDemo.java

--- risks/MASVS-STORAGE/backup-unencrypted/android-backup-unencrypted-use/example/InsecureStorageDemo.java
+++ risks/MASVS-STORAGE/backup-unencrypted/android-backup-unencrypted-use/example/InsecureStorageDemo.java
@@ -1,4 +1,3 @@
-// InsecureStorageDemo.java
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Environment;
@@ -8,19 +7,27 @@ import java.io.OutputStreamWriter;
 
 public class InsecureStorageDemo extends Context {
     public void insecureDataStorageMethod() {
-        SharedPreferences prefs = getSharedPreferences("user_prefs", MODE_PRIVATE);
         // Insecurely storing sensitive data in SharedPreferences without encryption
+        SharedPreferences prefs = getSharedPreferences("user_prefs", MODE_PRIVATE);
         prefs.edit().putString("authToken", "123456789").apply();
-        
+        prefs.edit().putString("password", "userPassword123").apply(); // Additional pattern
+
         // Attempting to store data in external storage without encryption
         try {
             File externalStorage = Environment.getExternalStorageDirectory();
-            File myFile = new File(externalStorage, "sensitiveData.txt");
-            FileOutputStream fos = new FileOutputStream(myFile);
+            File sensitiveFile = new File(externalStorage, "sensitiveData.txt");
+            FileOutputStream fos = new FileOutputStream(sensitiveFile);
             OutputStreamWriter osw = new OutputStreamWriter(fos);
             osw.write("Sensitive information, like passwords or personal info.");
             osw.close();
             fos.close();
+
+            // Additional insecure data write to external storage
+            FileOutputStream fos2 = new FileOutputStream(new File(externalStorage, "userData.txt"));
+            osw = new OutputStreamWriter(fos2);
+            osw.write("Usernames, email addresses, and other personal data.");
+            osw.close();
+            fos2.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
