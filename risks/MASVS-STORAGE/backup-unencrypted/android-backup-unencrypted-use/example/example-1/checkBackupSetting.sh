d39b34995163ab965deaf37eb3b161591f53a4b8
E.A <46949964+e-a-security@users.noreply.github.com>
2024-04-14 15:29:41 -0500

    Delete risks/MASVS-STORAGE/backup-unencrypted/android-backup-unencrypted-use/example/example-1 directory

--- risks/MASVS-STORAGE/backup-unencrypted/android-backup-unencrypted-use/example/example-1/checkBackupSetting.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Check for allowBackup setting in AndroidManifest.xml
-if grep 'android:allowBackup="true"' DisableBackupAndroidManifest.xml; then
-  echo "FAIL: Unencrypted backup allowed. Set allowBackup to false."
-else
-  echo "PASS: Backup is securely configured or disabled."
-fi
