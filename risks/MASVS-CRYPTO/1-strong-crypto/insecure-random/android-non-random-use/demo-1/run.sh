7d0a6da90fd219a5c1c22fc8bfec942dc1e0354e
cpholguera <perezholguera@gmail.com>
2024-05-14 13:51:28 +0200

    update demos

--- risks/MASVS-CRYPTO/1-strong-crypto/insecure-random/android-non-random-use/demo-1/run.sh
+++ risks/MASVS-CRYPTO/1-strong-crypto/insecure-random/android-non-random-use/demo-1/run.sh
@@ -1,2 +1,2 @@
-NO_COLOR=true semgrep -c ../rules/mastg-android-non-random-use.yaml ./non-random.java --text -o output.txt
-NO_COLOR=true semgrep -c ../rules/mastg-android-non-random-use.yaml ./non-random.java --sarif -o output.sarif
+NO_COLOR=true semgrep -c ../rules/mastg-android-non-random-use.yaml ./MastgTest.java --text -o output.txt
+NO_COLOR=true semgrep -c ../rules/mastg-android-non-random-use.yaml ./MastgTest.java --sarif -o output.sarif
