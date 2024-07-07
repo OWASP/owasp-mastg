1efab909f634ee30506acf3df1d1638321fadc0c
Carlos Holguera <perezholguera@gmail.com>
2024-07-07 10:40:17 +0200

    Update vscode debugging files, add support for tags and datatables (#2653)
    * add support for tags
    
    * add tasks and launch for debugging
    
    * remove launch.json from .gitignore
    
    * Add DataTables plugin for table functionality replacing tablesort

---
+++ docs/hooks/add-tags.py
@@ -0,0 +1,28 @@
+import logging
+import mkdocs.plugins
+
+log = logging.getLogger('mkdocs')
+
+# https://www.mkdocs.org/dev-guide/plugins/#on_page_markdown
+@mkdocs.plugins.event_priority(-50)
+def on_page_markdown(markdown, page, **kwargs):
+    path = page.file.src_uri
+
+    if "MASWE/" in path:
+
+        tags = page.meta.get('tags', [])
+
+        if page.meta.get('platform'): 
+            for platform in page.meta.get('platform', []):
+                tags.append(platform)
+        if page.meta.get('profiles'):
+            for profile in page.meta.get('profiles', []):
+                tags.append(profile)
+        
+        if page.meta.get('masvs-v2'):
+            for masvs_v2 in page.meta.get('masvs-v2', []):
+                tags.append(masvs_v2)
+        
+        page.meta['tags'] = tags
+
+    return markdown
