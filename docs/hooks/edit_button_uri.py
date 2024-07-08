6e8f8a15cccac1ea1fbcfd167d197ea8cd77bd20
Jeroen Beckers <dauntless@dauntless.be>
2024-07-08 12:34:13 +0200

    Remove edit btn from a few pages

--- docs/hooks/edit_button_uri.py
+++ docs/hooks/edit_button_uri.py
@@ -16,7 +16,11 @@ def on_pre_page(page, config, files):
     edit_url = edit_url.replace("master/Intro/", "master/Document/")
     edit_url = edit_url.replace("master/iOS/", "master/Document/")
     edit_url = edit_url.replace("master/Android/", "master/Document/")
-   
-    page.edit_url = edit_url
+    edit_url = edit_url.replace("master/MASWE/", "master/weaknesses/")
+    
+    if edit_url.endswith("/master/index.md") or edit_url.endswith("/master/MASVS/index.md"):
+        page.edit_url = ""
+    else:
+        page.edit_url = edit_url
  
     return page
