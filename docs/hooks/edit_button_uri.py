889c06eac28655b0814aefe57f7bfd69e94ceb9b
Jeroen Beckers <dauntless@dauntless.be>
2024-07-08 12:23:04 +0200

    Clean base

---
+++ docs/hooks/edit_button_uri.py
@@ -0,0 +1,22 @@
+import logging
+log = logging.getLogger('mkdocs')
+
+
+def on_pre_page(page, config, files):
+    try:
+        editor_url = config["editor_url"]
+    except KeyError:
+        return page
+
+    src_path = page.file.src_path
+    
+    edit_url = f"{editor_url}{src_path}"
+    edit_url = edit_url.replace("master/MASTG/", "master/")
+    edit_url = edit_url.replace("master/General/", "master/Document/")
+    edit_url = edit_url.replace("master/Intro/", "master/Document/")
+    edit_url = edit_url.replace("master/iOS/", "master/Document/")
+    edit_url = edit_url.replace("master/Android/", "master/Document/")
+   
+    page.edit_url = edit_url
+ 
+    return page
