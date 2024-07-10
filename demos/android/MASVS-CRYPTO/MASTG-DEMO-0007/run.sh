6584f6d7b3c331876ceab23c699d2443dd9df639
Carlos Holguera <perezholguera@gmail.com>
2024-07-10 07:48:00 +0200

    remove sarif files for now to simplify

--- demos/android/MASVS-CRYPTO/MASTG-DEMO-0007/run.sh
+++ demos/android/MASVS-CRYPTO/MASTG-DEMO-0007/run.sh
@@ -1,2 +1,1 @@
 NO_COLOR=true semgrep -c ../../../../rules/mastg-android-insecure-random-use.yaml ./MastgTest_reversed.java --text -o output.txt
-NO_COLOR=true semgrep -c ../../../../rules/mastg-android-insecure-random-use.yaml ./MastgTest_reversed.java --sarif -o output.sarif
