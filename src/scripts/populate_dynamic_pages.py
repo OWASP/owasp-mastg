5f425708361dbdd2af353d7e59d2d98502b48e0c
Carlos Holguera <perezholguera@gmail.com>
2024-07-13 12:30:28 +0200

    Merge tables in tests, tools, techniques

--- src/scripts/populate_dynamic_pages.py
+++ src/scripts/populate_dynamic_pages.py
@@ -64,7 +64,8 @@ def get_mastg_components_dict(name):
         
                     frontmatter = next(yaml.load_all(content, Loader=yaml.FullLoader))
                     component_id = os.path.splitext(os.path.basename(file))[0]
-                    frontmatter['id'] = f"[{component_id}](/{os.path.splitext(os.path.relpath(file, f"docs/"))[0]}/)"
+                    component_path = os.path.splitext(os.path.relpath(file, "docs/"))[0]
+                    frontmatter['id'] = f"[{component_id}](/{component_path})"
                     components.append(frontmatter)
         return components
 
@@ -139,28 +140,15 @@ def reorder_dict_keys(original_dict, key_order):
 
 # tests/index.md
 
-column_titles = {'id': 'ID', 'title': 'Name', 'masvs_v2_id': "MASVS v2 ID", 'masvs_v1_id': "MASVS v1 IDs", 'last_updated': 'Last Updated'} #'id': 'ID',  ... , 'refs': 'Refs', 'techniques': 'Techniques'
+column_titles = {'id': 'ID', 'title': 'Title', 'platform': "Platform", 'masvs_v2_id': "MASVS v2 ID", 'masvs_v1_id': "MASVS v1 IDs", 'last_updated': 'Last Updated'} #'id': 'ID',  ... , 'refs': 'Refs', 'techniques': 'Techniques'
 tests = get_mastg_components_dict("docs/MASTG/tests")
-test_types = ["android", "ios"]
-for test_type in test_types:
-    append_to_file(f"## {test_type.title()} tests\n\n<br>\n\n", "docs/MASTG/tests/index.md")
-    tests_of_type = [reorder_dict_keys(test, column_titles.keys()) for test in tests if test['platform'] == test_type]
-    for test in tests_of_type:
-        if test.get("masvs_v2_id"):
-            test['masvs_v2_id'] = test['masvs_v2_id'][0]
-        if test.get("masvs_v1_id"):
-            test['masvs_v1_id'] = "<br>".join([f"{v1_id}" for v1_id in test['masvs_v1_id']])
-    
-
-    for group_id, checklist in CHECKLIST_DICT.items():
-        append_to_file(f"### {group_id}\n\n<br>\n\n", "docs/MASTG/tests/index.md")
-
-        tests_by_category = [test for test in tests_of_type if test['masvs_v2_id'].startswith(group_id)]
-
-        # sort the dicts within tests_by_category by MASVS ID
-        tests_by_category.sort(key=lambda x: x['masvs_v2_id'])
-
-        append_to_file(list_of_dicts_to_md_table(tests_by_category, column_titles) + "\n\n<br>\n\n", "docs/MASTG/tests/index.md")
+tests_of_type = [reorder_dict_keys(test, column_titles.keys()) for test in tests]
+for test in tests_of_type:
+    if test.get("masvs_v2_id"):
+        test['masvs_v2_id'] = test['masvs_v2_id'][0]
+    if test.get("masvs_v1_id"):
+        test['masvs_v1_id'] = "<br>".join([f"{v1_id}" for v1_id in test['masvs_v1_id']])
+append_to_file(list_of_dicts_to_md_table(tests_of_type, column_titles) + "\n\n<br>\n\n", "docs/MASTG/tests/index.md")
 
 # tests-beta/index.md
 
@@ -185,35 +173,24 @@ append_to_file(list_of_dicts_to_md_table(demos_beta_columns_reordered, column_ti
 column_titles = {'id': 'ID', 'title': 'Name', 'platform': "Platform"} # TODO , 'refs': 'Refs', 'techniques': 'Techniques'
 
 tools = get_mastg_components_dict("docs/MASTG/tools")
-tool_types = ["generic", "android", "ios", "network"]
-for tool_type in tool_types:
-    append_to_file(f"## {tool_type.title()} Tools\n\n<br>\n\n", "docs/MASTG/tools/index.md")
-    tools_of_type = [reorder_dict_keys(tool, column_titles.keys()) for tool in tools if tool['platform'] == tool_type]
-    append_to_file(list_of_dicts_to_md_table(tools_of_type, column_titles) + "\n\n<br>\n\n", "docs/MASTG/tools/index.md")
+tools_of_type = [reorder_dict_keys(tool, column_titles.keys()) for tool in tools]
+append_to_file("\n" + list_of_dicts_to_md_table(tools_of_type, column_titles) + "\n\n<br>\n\n", "docs/MASTG/tools/index.md")
 
 # techniques/index.md
 
 column_titles = {'id': 'ID', 'title': 'Name', 'platform': "Platform"} # TODO , 'tools': 'Tools'
 
 techniques = get_mastg_components_dict("docs/MASTG/techniques")
-technique_types = ["generic", "android", "ios"]
-
-for technique_type in technique_types:
-    append_to_file(f"## {technique_type.title()} Techniques\n\n<br>\n\n", "docs/MASTG/techniques/index.md")
-    techniques_of_type = [reorder_dict_keys(technique, column_titles.keys()) for technique in techniques if technique['platform'] == technique_type]
-    append_to_file(list_of_dicts_to_md_table(techniques_of_type, column_titles) + "\n\n<br>\n\n", "docs/MASTG/techniques/index.md")
+techniques_of_type = [reorder_dict_keys(technique, column_titles.keys()) for technique in techniques]
+append_to_file(list_of_dicts_to_md_table(techniques_of_type, column_titles) + "\n\n<br>\n\n", "docs/MASTG/techniques/index.md")
 
 # apps/index.md
 
 column_titles = {'id': 'ID', 'title': 'Name', 'platform': "Platform"} # TODO , 'techniques': 'Used in'
 
 apps = get_mastg_components_dict("docs/MASTG/apps")
-app_types = ["android", "ios"]
-
-for app_type in app_types:
-    append_to_file(f"## {app_type.title()} Apps\n\n<br>\n\n", "docs/MASTG/apps/index.md")
-    apps_of_type = [reorder_dict_keys(app, column_titles.keys()) for app in apps if app['platform'] == app_type]
-    append_to_file(list_of_dicts_to_md_table(apps_of_type, column_titles) + "\n\n<br>\n\n", "docs/MASTG/apps/index.md")
+apps_of_type = [reorder_dict_keys(app, column_titles.keys()) for app in apps]
+append_to_file(list_of_dicts_to_md_table(apps_of_type, column_titles) + "\n\n<br>\n\n", "docs/MASTG/apps/index.md")
 
 # weaknesses/index.md
 
