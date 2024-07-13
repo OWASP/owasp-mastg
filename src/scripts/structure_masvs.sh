de7f78d4ceeea4389a240133992f6b8a4362192f
Jeroen Beckers <dauntless@dauntless.be>
2024-07-13 13:41:24 +0200

    debu

--- src/scripts/structure_masvs.sh
+++ src/scripts/structure_masvs.sh
@@ -1,14 +1,14 @@
-
-if [ ! -d "../owasp-masvs/" ] ; then
+echo "$PWD"
+if [ ! -d "../../owasp-masvs/" ] ; then
   echo "Error: Clone owasp-masvs to same directory as owasp-mastg"
   exit
 fi
 
 mkdir -p docs/MASVS/Intro
 mkdir -p docs/MASVS/controls
-cp ../owasp-masvs/Document/*.md docs/MASVS
+cp ../../owasp-masvs/Document/*.md docs/MASVS
 mv docs/MASVS/0[1-4]*.md docs/MASVS/Intro
-cp ../owasp-masvs/controls/* docs/MASVS/controls
+cp ../../owasp-masvs/controls/* docs/MASVS/controls
 
 if [[ "$(uname)" == "Darwin" ]]; then
     SED="gsed"
@@ -18,6 +18,6 @@ fi
 
 
 mkdir -p docs/assets/Images/MASVS
-cp ../owasp-masvs/Document/images/* docs/assets/Images/MASVS
+cp ../../owasp-masvs/Document/images/* docs/assets/Images/MASVS
 $SED -i "s#images/#../../../assets/Images/MASVS/#g" docs/MASVS/**/*.md
 $SED -i "s#images/#../../assets/Images/MASVS/#g" docs/MASVS/*.md
