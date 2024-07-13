697006ffe9aa057260ac586d1a9f32070993c9c8
Jeroen Beckers <info@dauntless.be>
2024-07-13 14:29:02 +0200

    Fix GitHub flow bug (#2809)
    * Test
    
    * Fix relative path again
    
    * Weird test
    
    ---------
    
    Co-authored-by: Jeroen Beckers <dauntless@dauntless.be>

--- src/scripts/structure_masvs.sh
+++ src/scripts/structure_masvs.sh
@@ -1,14 +1,17 @@
-echo "$PWD"
-if [ ! -d "../owasp-masvs/" ] ; then
-  echo "Error: Please clone owasp-masvs to same directory as owasp-mastg"
-  exit
+if [ -d "../owasp-masvs/" ] ; then
+    MASVS_DIR=../owasp-masvs
+elif [ ! -d "../owasp-masvs/" ] ; then
+    MASVS_DIR=./owasp-masvs
+else
+    echo "Error: Please clone owasp-masvs to same directory as owasp-mastg"
+    exit
 fi
 
 mkdir -p docs/MASVS/Intro
 mkdir -p docs/MASVS/controls
-cp ../owasp-masvs/Document/*.md docs/MASVS
+cp $MASVS_DIR/Document/*.md docs/MASVS
 mv docs/MASVS/0[1-4]*.md docs/MASVS/Intro
-cp ../owasp-masvs/controls/* docs/MASVS/controls
+cp $MASVS_DIR/controls/* docs/MASVS/controls
 
 if [[ "$(uname)" == "Darwin" ]]; then
     SED="gsed"
@@ -18,6 +21,6 @@ fi
 
 
 mkdir -p docs/assets/Images/MASVS
-cp ../owasp-masvs/Document/images/* docs/assets/Images/MASVS
+cp $MASVS_DIR/Document/images/* docs/assets/Images/MASVS
 $SED -i "s#images/#../../../assets/Images/MASVS/#g" docs/MASVS/**/*.md
 $SED -i "s#images/#../../assets/Images/MASVS/#g" docs/MASVS/*.md
