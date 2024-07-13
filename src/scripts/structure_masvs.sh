eab8800f1d0eb60ba3e14e74be7d3f9b636485f7
Carlos Holguera <perezholguera@gmail.com>
2024-07-13 14:58:17 +0200

    Improve structuring scripts and flatten nav (#2803)
    * optimize structure scripts and flatten nav for MASTG
    
    * Optimize structure scripts and flatten nav for MASVS
    
    ---------
    
    Co-authored-by: Jeroen Beckers <info@dauntless.be>

--- src/scripts/structure_masvs.sh
+++ src/scripts/structure_masvs.sh
@@ -1,24 +1,20 @@
 if [ -d "../owasp-masvs/" ] ; then
     MASVS_DIR=../owasp-masvs
-elif [ ! -d "../owasp-masvs/" ] ; then
+elif [ -d "./owasp-masvs/" ] ; then
     MASVS_DIR=./owasp-masvs
 else
-    echo "Error: Please clone owasp-masvs to same directory as owasp-mastg"
-    exit
+    echo "Error: Please clone owasp-masvs to same directory as owasp-mastg: cd .. && git clone https://github.com/OWASP/owasp-masvs.git"
+    exit 1
 fi
 
-mkdir -p docs/MASVS/Intro
-mkdir -p docs/MASVS/controls
-cp $MASVS_DIR/Document/*.md docs/MASVS
-mv docs/MASVS/0[1-4]*.md docs/MASVS/Intro
-cp $MASVS_DIR/controls/* docs/MASVS/controls
+cp -r $MASVS_DIR/Document/*-*.md docs/MASVS/
+cp -r $MASVS_DIR/controls/ docs/MASVS/controls/
 
 if [[ "$(uname)" == "Darwin" ]]; then
     SED="gsed"
 else
     SED="sed"
 fi
-
 
 mkdir -p docs/assets/Images/MASVS
 cp $MASVS_DIR/Document/images/* docs/assets/Images/MASVS
