356d44f1c3236e23ccc38255ff37d0932cab0de4
TheDauntless <info@dauntless.be>
2024-07-15 10:22:10 +0200

    Move genere_cross_references

--- docs/hooks/add-cross-references.py
+++ docs/hooks/add-cross-references.py
@@ -1,12 +1,72 @@
 import logging
 import yaml
 import mkdocs.plugins
+import os
+import glob
 
 log = logging.getLogger('mkdocs')
 
-def load_cross_references():
-    with open("cross_references.yaml", 'r') as f:
-        return yaml.load(f, Loader=yaml.FullLoader)
+
+def gather_metadata(directory, id_key):
+    metadata = {}
+    for file in glob.glob(f"./docs/{directory}/**/MASWE-*.md", recursive=True):
+        if file.endswith("index.md"):
+            continue
+
+        with open(file, 'r') as f:
+            content = f.read()
+            frontmatter = next(yaml.load_all(content, Loader=yaml.FullLoader))
+            # path is file without the .md extension, 'weaknesses/MASVS-STORAGE/MASWE-0004.md' -> weaknesses/MASVS-STORAGE/MASWE-0004
+            
+            if not id_key in frontmatter:
+                log.error(f"Missing frontmatter ID in {file}")
+                continue
+
+            frontmatter["path"] = file[:-3]
+            # replace weaknesses with MASWE in path
+            frontmatter["path"] = frontmatter["path"].replace("weaknesses", "/MASWE")
+            if directory == "tests-beta" or directory == "demos":
+                frontmatter["path"] = frontmatter["path"].replace("tests-beta", "/MASTG/tests-beta")
+                frontmatter["path"] = frontmatter["path"].replace("demos", "/MASTG/demos") 
+
+            metadata[frontmatter[id_key]] = frontmatter
+    return metadata
+
+def generate_cross_references():
+    weaknesses = gather_metadata("MASWE", "id")
+    tests = gather_metadata("tests-beta", "id")
+    demos = gather_metadata("demos", "id")
+
+    cross_references = {
+        "weaknesses": {},
+        "tests": {}
+    }
+
+    for test_id, test_meta in tests.items():
+        weakness_id = test_meta.get("weakness")
+        test_path = test_meta.get("path")
+        test_title = test_meta.get("title")
+        test_platform = test_meta.get("platform")
+        if weakness_id:
+            if weakness_id not in cross_references["weaknesses"]:
+                cross_references["weaknesses"][weakness_id] = []
+            cross_references["weaknesses"][weakness_id].append({"id": test_id, "path": test_path, "title": test_title, "platform": test_platform})
+
+    for demo_id, demo_meta in demos.items():
+        test_id = demo_meta.get("test")
+        demo_path = demo_meta.get("path")
+        demo_title = demo_meta.get("title")
+        demo_platform = demo_meta.get("platform")
+        if test_id:
+            if test_id not in cross_references["tests"]:
+                cross_references["tests"][test_id] = []
+            cross_references["tests"][test_id].append({"id": demo_id, "path": demo_path, "title": demo_title, "platform": demo_platform})
+
+
+    with open("cross_references.yaml", 'w') as f:
+        yaml.dump(cross_references, f)
+        
+    return cross_references
 
 def get_platform_icon(platform):
     if platform == "ios":
@@ -15,12 +75,15 @@ def get_platform_icon(platform):
         return ":material-android:"
     return ":material-asterisk:"
 
-cross_references = load_cross_references()
+def on_pre_build(config):
+    config.cross_references = generate_cross_references()
 
 @mkdocs.plugins.event_priority(-50)
-def on_page_markdown(markdown, page, **kwargs):
+def on_page_markdown(markdown, page, config, **kwargs):
     path = page.file.src_uri
     meta = page.meta
+
+    cross_references = config.cross_references
 
     if "MASWE-" in path:
         weakness_id = meta.get('id')
