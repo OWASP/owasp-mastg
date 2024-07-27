3d4391e7c8b9f8637f849f9319e8cf7a35b8020d
Carlos Holguera <perezholguera@gmail.com>
2024-07-27 10:59:34 +0200

    feat: Refactor edit button URI handling code into a function

--- docs/hooks/edit_button_uri.py
+++ docs/hooks/edit_button_uri.py
@@ -1,6 +1,26 @@
 import logging
+
 log = logging.getLogger('mkdocs')
 
+def get_edit_url(src_path, edit_url_mastg, edit_url_masvs):
+    if src_path.startswith("MASVS"):
+        edit_url = f"{edit_url_masvs}{src_path}"
+        edit_url = edit_url.replace("master/MASVS/controls", "master/controls/")
+        edit_url = edit_url.replace("master/MASVS/", "master/Document/")
+    elif src_path.startswith("MASTG"):
+        edit_url = f"{edit_url_mastg}{src_path}"
+        edit_url = edit_url.replace("master/MASTG/0x", "master/Document/0x")
+        edit_url = edit_url.replace("master/MASTG/", "master/")
+    elif src_path.startswith("MASWE"):
+        edit_url = f"{edit_url_mastg}{src_path}"
+        edit_url = edit_url.replace("master/MASWE/", "master/weaknesses/")
+    elif src_path.startswith(("contributing", "donate")):
+        edit_url = f"{edit_url_mastg}{src_path}"
+        edit_url = edit_url.replace("master/", "master/docs/")
+    else:
+        edit_url = ""
+    
+    return edit_url
 
 def on_pre_page(page, config, files):
     try:
@@ -11,32 +31,8 @@ def on_pre_page(page, config, files):
     
     src_path = page.file.src_path
 
-    # run only if src_path starts with "MASTG", "MASVS", "MASWE", "contributing" or "donate"
     if src_path.startswith(("MASTG", "MASVS", "MASWE", "contributing", "donate")):
-    
-        edit_url = edit_url_mastg
-
-        # MASVS is in a different repo, so need a different editor url
-        if src_path.startswith("MASVS"):
-            edit_url = edit_url_masvs
-            edit_url = f"{edit_url}{src_path}"
-
-            edit_url = edit_url.replace("master/MASVS/controls", "master/controls/")
-            edit_url = edit_url.replace("master/MASVS/", "master/Document/")
-
-        elif src_path.startswith("MASTG") or src_path.startswith("MASWE"):
-            edit_url = f"{edit_url}{src_path}"
-            # Rewrite some URLs
-            edit_url = edit_url.replace("master/MASTG/0x", "master/Document/0x")
-            edit_url = edit_url.replace("master/MASTG/", "master/")
-            edit_url = edit_url.replace("master/MASWE/", "master/weaknesses/")
-        
-        elif src_path.startswith("contributing") or src_path.startswith("donate"):
-            edit_url = f"{edit_url}{src_path}"
-            edit_url = edit_url.replace("master/", "master/docs/")
-
-        # index.md files are mostly auto-generated and an edit link would result in a 404
-        # Setting it to empty prevents the button from being added
+        edit_url = get_edit_url(src_path, edit_url_mastg, edit_url_masvs)
         if edit_url.endswith("/index.md"):
             page.edit_url = ""
         else:
