cff5857632f488996f8cce2107e80315e09499f0
Carlos Holguera <perezholguera@gmail.com>
2024-07-27 10:54:32 +0200

    feat: fix universal edit button functionality and restrict for specific file paths

--- docs/hooks/edit_button_uri.py
+++ docs/hooks/edit_button_uri.py
@@ -10,33 +10,38 @@ def on_pre_page(page, config, files):
         return page
     
     src_path = page.file.src_path
+
+    # run only if src_path starts with "MASTG", "MASVS", "MASWE", "contributing" or "donate"
+    if src_path.startswith(("MASTG", "MASVS", "MASWE", "contributing", "donate")):
     
-    edit_url = edit_url_mastg
+        edit_url = edit_url_mastg
 
-    # MASVS is in a different repo, so need a different editor url
-    if src_path.startswith("MASVS"):
-        edit_url = edit_url_masvs
-        edit_url = f"{edit_url}{src_path}"
+        # MASVS is in a different repo, so need a different editor url
+        if src_path.startswith("MASVS"):
+            edit_url = edit_url_masvs
+            edit_url = f"{edit_url}{src_path}"
 
-        edit_url = edit_url.replace("master/MASVS/controls", "master/controls/")
-        edit_url = edit_url.replace("master/MASVS/", "master/Document/")
+            edit_url = edit_url.replace("master/MASVS/controls", "master/controls/")
+            edit_url = edit_url.replace("master/MASVS/", "master/Document/")
 
-    else:
-        edit_url = f"{edit_url}{src_path}"
-        # Rewrite some URLs
-        edit_url = edit_url.replace("master/MASTG/", "master/")
-        edit_url = edit_url.replace("master/General/", "master/Document/")
-        edit_url = edit_url.replace("master/Intro/", "master/Document/")
-        edit_url = edit_url.replace("master/iOS/", "master/Document/")
-        edit_url = edit_url.replace("master/Android/", "master/Document/")
-        edit_url = edit_url.replace("master/MASWE/", "master/weaknesses/")
+        elif src_path.startswith("MASTG") or src_path.startswith("MASWE"):
+            edit_url = f"{edit_url}{src_path}"
+            # Rewrite some URLs
+            edit_url = edit_url.replace("master/MASTG/0x", "master/Document/0x")
+            edit_url = edit_url.replace("master/MASTG/", "master/")
+            edit_url = edit_url.replace("master/MASWE/", "master/weaknesses/")
         
+        elif src_path.startswith("contributing") or src_path.startswith("donate"):
+            edit_url = f"{edit_url}{src_path}"
+            edit_url = edit_url.replace("master/", "master/docs/")
 
-    # index.md files are mostly auto-generated and an edit link would result in a 404
-    # Setting it to empty prevents the button from being added
-    if edit_url.endswith("/index.md"):
-        page.edit_url = ""
+        # index.md files are mostly auto-generated and an edit link would result in a 404
+        # Setting it to empty prevents the button from being added
+        if edit_url.endswith("/index.md"):
+            page.edit_url = ""
+        else:
+            page.edit_url = edit_url
     else:
-        page.edit_url = edit_url
+        page.edit_url = ""
  
     return page
