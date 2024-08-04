9bc907ba03b8958145fdf64265ca5bb4f7cf34fa
Carlos Holguera <perezholguera@gmail.com>
2024-08-04 12:02:41 +0200

    update icons

--- docs/hooks/resolve_references.py
+++ docs/hooks/resolve_references.py
@@ -12,68 +12,63 @@ mapping = {"TECH":{}, "TOOL":{}, "TEST": {}, "APP": {}, "MASWE": {}, "MASVS": {}
 def on_page_markdown(markdown, page, **kwargs):
     path = page.file.src_uri
 
-    # Always true, but nice for debugging
-    if True:
+    pageRefs = {"TECH":[], "TOOL":[], "TEST": [], "APP": [], "MASWE": [], "MASVS": [], "DEMO": []}
+    def replaceReference(match):
+        refType = match.group(2)
 
-        pageRefs = {"TECH":[], "TOOL":[], "TEST": [], "APP": [], "MASWE": [], "MASVS": [], "DEMO": []}
-        def replaceReference(match):
-            refType = match.group(2)
+        pageRefs[refType].append(match.group(1))
 
-            pageRefs[refType].append(match.group(1))
+        if not match in mapping[refType]:
+            target = getTargetForRef(match.group(1), path)
+            mapping[refType][match] = target
 
-            if not match in mapping[refType]:
-                target = getTargetForRef(match.group(1), path)
-                mapping[refType][match] = target
+        icon = ""
+        if refType == "TOOL":
+            icon = ":octicons-tools-24: "
+        elif refType == "TEST":
+            icon = ":octicons-codescan-checkmark-24: "
+        elif refType == "APP":
+            icon = ":octicons-code-square-24: "
+        elif refType == "DEMO":
+            icon = ":material-flask-outline: "
+        elif refType == "TECH":
+            icon = ":material-magic-staff: "
 
-            icon = ""
-            if refType == "TOOL":
-                icon = ":material-tools: "
-            elif refType == "TEST":
-                icon = ":octicons-checklist-24: "
-            elif refType == "APP":
-                icon = ":fontawesome-solid-mobile-screen-button: "
-            elif refType == "DEMO":
-                icon = ":material-presentation: "
-            else:
-                icon = ":material-flask: "
+        return f"_[{icon}{mapping[refType][match]['title']}]({mapping[refType][match]['file']})_"
 
-            return f"_[{icon}{mapping[refType][match]['title']}]({mapping[refType][match]['file']})_"
+    def replaceReferenceMASWE(match):
+        refType = "MASWE"
 
-        def replaceReferenceMASWE(match):
-            refType = "MASWE"
+        pageRefs[refType].append(match.group(1))
 
-            pageRefs[refType].append(match.group(1))
+        if not match in mapping[refType]:
+            target = getTargetForRef(match.group(1), path)
+            mapping[refType][match] = target
 
-            if not match in mapping[refType]:
-                target = getTargetForRef(match.group(1), path)
-                mapping[refType][match] = target
+        icon = ":material-skull-scan-outline: "
+        return f"_[{icon}{mapping[refType][match]['title']}]({mapping[refType][match]['file']})_"
 
-            icon = ":fontawesome-solid-link-slash: "
-            return f"_[{icon}{mapping[refType][match]['title']}]({mapping[refType][match]['file']})_"
+    def replaceReferenceMASVS(match):
+        refType = "MASVS"
 
-        def replaceReferenceMASVS(match):
-            refType = "MASVS"
+        pageRefs[refType].append(match.group(1))
 
-            pageRefs[refType].append(match.group(1))
+        if not match in mapping[refType]:
+            target = getTargetForRef(match.group(1), path)
+            mapping[refType][match] = target
 
-            if not match in mapping[refType]:
-                target = getTargetForRef(match.group(1), path)
-                mapping[refType][match] = target
-
-            icon = ":material-book-multiple: "
-            return f"_[{icon}{mapping[refType][match]['title']}]({mapping[refType][match]['file']})_"
+        icon = ":simple-owasp: "
+        return f"_[{icon}{mapping[refType][match]['title']}]({mapping[refType][match]['file']})_"
 
 
-        updated_markdown = re.sub(r'@(MASTG-(TECH|TOOL|TEST|APP|DEMO)-\d{3,})', replaceReference, markdown)
-        updated_markdown = re.sub(r'@(MASWE-\d{3,})', replaceReferenceMASWE, updated_markdown)
-        updated_markdown = re.sub(r'@(MASVS-\w{})', replaceReferenceMASVS, updated_markdown)
-        tags = page.meta.get('tags', [])
-        page.meta["tools"] = list(set(pageRefs["TOOL"]))
-        page.meta["techniques"] = list(set(pageRefs["TECH"]))
+    updated_markdown = re.sub(r'@(MASTG-(TECH|TOOL|TEST|APP|DEMO)-\d{3,})', replaceReference, markdown)
+    updated_markdown = re.sub(r'@(MASWE-\d{3,})', replaceReferenceMASWE, updated_markdown)
+    updated_markdown = re.sub(r'@(MASVS-\w+)', replaceReferenceMASVS, updated_markdown)
+    tags = page.meta.get('tags', [])
+    page.meta["tools"] = list(set(pageRefs["TOOL"]))
+    page.meta["techniques"] = list(set(pageRefs["TECH"]))
 
-        return updated_markdown
-
-    return markdown
+    return updated_markdown
 
 def getTargetForRef(id, path):
     searchFor = f'./docs/**/{id}.md'
