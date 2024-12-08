a025d7d42d5ef44728ebdb87e650b8201901bd83
Carlos Holguera <perezholguera@gmail.com>
2024-12-08 20:40:50 +0100

    refactor: clean up MastgTestWebView class and improve SSL certificate handling

--- demos/android/MASVS-NETWORK/MASTG-DEMO-xxx2/MastgTestWebView.kt
+++ demos/android/MASVS-NETWORK/MASTG-DEMO-xxx2/MastgTestWebView.kt
@@ -5,16 +5,16 @@ import android.net.http.SslCertificate
 import android.net.http.SslError
 import android.util.Log
 import android.webkit.SslErrorHandler
-
+import android.webkit.WebView
 import android.webkit.WebViewClient
 import java.io.ByteArrayInputStream
 import java.security.cert.CertificateFactory
 import java.security.cert.X509Certificate
 
-class MastgTestWebView (private val context: Context){
+class MastgTestWebView(private val context: Context) {
 
     fun mastgTest(webView: WebView) {
-        webView?.apply {
+        webView.apply {
             webViewClient = object : WebViewClient() {
                 override fun onReceivedSslError(
                     view: WebView?,
@@ -28,7 +28,13 @@ class MastgTestWebView (private val context: Context){
                         val pinnedCert = certificateFactory.generateCertificate(pinnedCertInputStream)
 
                         // Extract the server's certificate
-                        val serverCert = error?.certificate?.toX509Certificate()
+                        val serverCert = error?.certificate?.let { sslCertificate ->
+                            val bundle = SslCertificate.saveState(sslCertificate)
+                            val bytes = bundle.getByteArray("x509-certificate")
+                            bytes?.let {
+                                certificateFactory.generateCertificate(ByteArrayInputStream(it)) as? X509Certificate
+                            }
+                        }
 
                         // Compare the certificates
                         if (serverCert != null && serverCert == pinnedCert) {
@@ -48,19 +54,4 @@ class MastgTestWebView (private val context: Context){
             loadUrl("https://self-signed.badssl.com/")  // Certificates match, proceed
         }
     }
-
-    /**
-     * Extension function to convert SslCertificate to X509Certificate
-     */
-    fun SslCertificate.toX509Certificate(): X509Certificate? {
-        return try {
-            val bundle = SslCertificate.saveState(this)
-            val bytes = bundle.getByteArray("x509-certificate")
-            val certificateFactory = CertificateFactory.getInstance("X.509")
-            certificateFactory.generateCertificate(ByteArrayInputStream(bytes)) as? X509Certificate
-        } catch (e: Exception) {
-            null
-        }
-    }
-
 }
