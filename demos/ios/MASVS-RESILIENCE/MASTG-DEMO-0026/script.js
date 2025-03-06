6faea7304db0031918d3453dc96c4abb40d8ac36
Carlos Holguera <perezholguera@gmail.com>
2025-03-06 19:54:50 +0100

    Enhance demo ID MASTG-DEMO-0026 to log intercepted LAContext.canEvaluatePolicy calls with detailed backtrace

--- demos/ios/MASVS-RESILIENCE/MASTG-DEMO-0026/script.js
+++ demos/ios/MASVS-RESILIENCE/MASTG-DEMO-0026/script.js
@@ -1,5 +1,20 @@
 Interceptor.attach(ObjC.classes.LAContext["- canEvaluatePolicy:error:"].implementation, {
-    onEnter(args) {
-      console.log("LAcontext.canEvaluatePolicy("+args[2]+")");
-    }
+  onEnter(args) {
+
+      const LAPolicy = {
+          1: ".deviceOwnerAuthenticationWithBiometrics",
+          2: ".deviceOwnerAuthentication"
+      };
+
+      const policy = args[2].toInt32();
+
+      const policyDescription = LAPolicy[policy] || "Unknown Policy";
+
+      console.log("Intercepted: LAContext.canEvaluatePolicy(" + args[2] + ") # " + args[2] + " = " + policyDescription);
+
+      // Get the caller backtrace
+      console.log("\nBacktrace:\n" + Thread.backtrace(this.context, Backtracer.ACCURATE)
+          .map(DebugSymbol.fromAddress).join("\n"));
+
+  }
 });
