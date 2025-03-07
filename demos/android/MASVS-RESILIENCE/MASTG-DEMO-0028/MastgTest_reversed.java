da50a57bb88050fa3908b347669bf3cedff538de
Carlos Holguera <perezholguera@gmail.com>
2025-03-07 12:05:27 +0100

    Add AndroidManifest.xml and reversed version for MASTG-DEMO-0028 with necessary permissions and application configuration

--- demos/android/MASVS-RESILIENCE/MASTG-DEMO-0028/MastgTest_reversed.java
+++ demos/android/MASVS-RESILIENCE/MASTG-DEMO-0028/MastgTest_reversed.java
@@ -8,7 +8,7 @@ import kotlin.Metadata;
 import kotlin.jvm.internal.Intrinsics;
 
 /* compiled from: MastgTest.kt */
-@Metadata(m69d1 = {"\u0000 \n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0010\u000e\n\u0000\n\u0002\u0010\u000b\n\u0002\b\u0002\b\u0007\u0018\u00002\u00020\u0001B\r\u0012\u0006\u0010\u0002\u001a\u00020\u0003¢\u0006\u0002\u0010\u0004J\u0006\u0010\u0005\u001a\u00020\u0006J\u000e\u0010\u0007\u001a\u00020\b2\u0006\u0010\u0002\u001a\u00020\u0003J\u0006\u0010\t\u001a\u00020\u0006R\u000e\u0010\u0002\u001a\u00020\u0003X\u0082\u0004¢\u0006\u0002\n\u0000¨\u0006\n"}, m70d2 = {"Lorg/owasp/mastestapp/MastgTest;", "", "context", "Landroid/content/Context;", "(Landroid/content/Context;)V", "checkStrongBiometricStatus", "", "isDeviceSecure", "", "mastgTest", "app_debug"}, m71k = 1, m72mv = {1, 9, 0}, m74xi = 48)
+@Metadata(d1 = {"\u0000 \n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0010\u000e\n\u0000\n\u0002\u0010\u000b\n\u0002\b\u0002\b\u0007\u0018\u00002\u00020\u0001B\r\u0012\u0006\u0010\u0002\u001a\u00020\u0003¢\u0006\u0002\u0010\u0004J\u0006\u0010\u0005\u001a\u00020\u0006J\u000e\u0010\u0007\u001a\u00020\b2\u0006\u0010\u0002\u001a\u00020\u0003J\u0006\u0010\t\u001a\u00020\u0006R\u000e\u0010\u0002\u001a\u00020\u0003X\u0082\u0004¢\u0006\u0002\n\u0000¨\u0006\n"}, d2 = {"Lorg/owasp/mastestapp/MastgTest;", "", "context", "Landroid/content/Context;", "(Landroid/content/Context;)V", "checkStrongBiometricStatus", "", "isDeviceSecure", "", "mastgTest", "app_debug"}, k = 1, mv = {1, 9, 0}, xi = 48)
 /* loaded from: classes4.dex */
 public final class MastgTest {
     public static final int $stable = 8;
