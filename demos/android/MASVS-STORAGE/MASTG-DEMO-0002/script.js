2f6809a911ad64282e80643366fc9e317ed31c23
Carlos Holguera <perezholguera@gmail.com>
2025-03-07 18:25:29 +0100

    Enhance script.js in MASTG-DEMO-0002 to improve backtrace logging and external storage access monitoring

--- demos/android/MASVS-STORAGE/MASTG-DEMO-0002/script.js
+++ demos/android/MASVS-STORAGE/MASTG-DEMO-0002/script.js
@@ -1,27 +1,35 @@
-// Intercept libc`open to make sure we cover all Java's APIs
+// It calls Java.performNow to ensure Java classes are available.
+function printBacktrace(maxLines = 8) {
+    Java.performNow(() => {
+        let Exception = Java.use("java.lang.Exception");
+        let stackTrace = Exception.$new().getStackTrace().toString().split(",");
+        console.log("\nBacktrace:");
+        for (let i = 0; i < Math.min(maxLines, stackTrace.length); i++) {
+            console.log(stackTrace[i]);
+        }
+    });
+}
+
+Java.perform(() => {
+    let ContentResolver = Java.use("android.content.ContentResolver");
+    ContentResolver["insert"].overload('android.net.Uri', 'android.content.ContentValues').implementation = function(uri, values) {
+        let result = this.insert(uri, values);
+        console.log(`\n[*] ContentResolver.insert called to open a file via MediaStore at: ${result}`);
+        printBacktrace();
+        return result;
+    };
+
+});
+
 Interceptor.attach(Module.getExportByName(null, 'open'), {
-    onEnter(args) {
-        const external_paths = ['/sdcard', '/storage/emulated']
+    onEnter: function(args) {
+        const external_paths = ['/sdcard', '/storage/emulated'];
         const path = Memory.readCString(ptr(args[0]));
         external_paths.forEach(external_path => {
-            if(path.indexOf(external_path) == 0){
-                console.log('[WARNING] Opening a file from external storage at:', path)
-                Java.performNow(function() {
-                    console.log("Invoked from: "+Java.use("android.util.Log").getStackTraceString(Java.use("java.lang.Exception").$new()))
-                });
+            if (path.indexOf(external_path) === 0) {
+                console.log(`\n[*] open called to open a file from external storage at: ${path}`);
+                printBacktrace(15);
             }
         });
     }
-});
-
-Java.perform(function() {
-    let ContentResolver = Java.use("android.content.ContentResolver");
-    ContentResolver["insert"].overload('android.net.Uri', 'android.content.ContentValues').implementation = function (uri, values) {
-        var result = this.insert(uri, values);
-        console.log('[WARNING] Opening a file with MediaStore at:', result)
-        Java.performNow(function() {
-            console.log("Invoked from: "+Java.use("android.util.Log").getStackTraceString(Java.use("java.lang.Exception").$new()))
-        });
-        return result;
-    };
 });
