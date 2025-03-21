fddc3e08854a79e8f24f2b9e4d3e41ad27dceb32
Carlos Holguera <perezholguera@gmail.com>
2025-03-21 17:49:55 +0100

    Remove unnecessary output option from semgrep command in run.sh

--- demos/android/MASVS-STORAGE/MASTG-DEMO-0024/run.sh
+++ demos/android/MASVS-STORAGE/MASTG-DEMO-0024/run.sh
@@ -1,1 +1,1 @@
-NO_COLOR=true semgrep -c ../../../../rules/mastg-android-find-non-caching-input-types.yml ./MastgTest_reversed.java --text -o output.txt
+NO_COLOR=true semgrep -c ../../../../rules/mastg-android-find-non-caching-input-types.yml ./MastgTest_reversed.java > output.txt
