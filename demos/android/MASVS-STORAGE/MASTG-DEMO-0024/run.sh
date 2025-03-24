d5aa956fe3e2ac36be9bd861887d007083b994f9
Carlos Holguera <perezholguera@gmail.com>
2025-03-24 10:33:41 +0100

    rename rule

--- demos/android/MASVS-STORAGE/MASTG-DEMO-0024/run.sh
+++ demos/android/MASVS-STORAGE/MASTG-DEMO-0024/run.sh
@@ -1,1 +1,1 @@
-NO_COLOR=true semgrep -c ../../../../rules/mastg-android-non-caching-input-types.yml ./MastgTest_reversed.java > output.txt
+NO_COLOR=true semgrep -c ../../../../rules/mastg-android-keyboard-cache-input-types.yml ./MastgTest_reversed.java > output.txt
