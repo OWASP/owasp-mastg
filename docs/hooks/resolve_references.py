5e63840788535b02ab442e40c8dd2da4f6056fa0
Jeroen Beckers <dauntless@dauntless.be>
2025-06-11 16:42:18 +0200

    Further improve performance

--- docs/hooks/resolve_references.py
+++ docs/hooks/resolve_references.py
@@ -4,7 +4,7 @@ import mkdocs.plugins
 import os
 from glob import glob
 import yaml
-
+from functools import lru_cache
 log = logging.getLogger('mkdocs')
 
 mapping = {"TECH":{}, "TOOL":{}, "TEST": {}, "APP": {}, "MASWE": {}, "MASVS": {}, "DEMO": {}, "BEST": {}}
@@ -13,9 +13,8 @@ mapping = {"TECH":{}, "TOOL":{}, "TEST": {}, "APP": {}, "MASWE": {}, "MASVS": {}
 def on_page_markdown(markdown, page, config, **kwargs):
     path = page.file.src_uri
 
-    icons = config.get('theme').get('icon').get('tag', {})
-
-    icons_for_text = {key.upper(): f":{value.replace('/', '-')}: " for key, value in icons.items()}
+    icons = config.get('resolve_ref_icons')
+    icons_for_text = config.get('resolve_ref_icons_for_text')
 
     pageRefs = {"TECH": [], "TOOL": [], "TEST": [], "APP": [], "MASWE": [], "MASVS": [], "DEMO": [], "BEST": []}
 
@@ -66,6 +65,15 @@ def on_page_markdown(markdown, page, config, **kwargs):
 
     return updated_markdown
 
+@lru_cache(maxsize=None)
+def getFileContent(path):
+    with open(path, 'r') as f:
+        content = f.read()
+        metadata = next(yaml.safe_load_all(content))
+
+        return {"title": metadata['title']}
+
+@lru_cache(maxsize=None)
 def getTargetForRef(id, path):
     searchFor = f'./docs/**/{id}.md'
 
@@ -74,11 +82,12 @@ def getTargetForRef(id, path):
     if not len(files):
         log.error("Unknown reference: " + id)
         return {"file": "ERROR", "title": "error"}
-    
+
     file_url =   os.path.relpath(files[0], "./docs/" + os.path.dirname(path))
+    data = getFileContent(files[0])
+    data["file"] = file_url
+    return data
 
-    with open(files[0], 'r') as f:
-        content = f.read()
-        metadata = next(yaml.safe_load_all(content))
-
-        return {"file": file_url, "title": metadata['title']}
+def on_config(config):
+    config["resolve_ref_icons"] = config.get('theme').get('icon').get('tag', {})
+    config["resolve_ref_icons_for_text"] = {key.upper(): f":{value.replace('/', '-')}: " for key, value in config["resolve_ref_icons"].items()}
