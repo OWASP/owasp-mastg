f582a8d4d1caa3ac0518fe9a5da75f8152705389
Jeroen Beckers <dauntless@dauntless.be>
2025-06-11 10:44:45 +0200

    Sanity check

--- docs/hooks/extract-id-from-path.py
+++ docs/hooks/extract-id-from-path.py
@@ -5,7 +5,7 @@ log = logging.getLogger('mkdocs')
 
 # This plugin extracts the ID from the filename if no ID is defined
 # This is only the case for older tests (ID < 200)
-@mkdocs.plugins.event_priority(-49)
+@mkdocs.plugins.event_priority(30)
 def on_page_markdown(markdown, page, config, **kwargs):
     path = page.file.src_uri
 
@@ -16,6 +16,9 @@ def on_page_markdown(markdown, page, config, **kwargs):
             raise Exception(f"Unable to extract ID from path: '{path}'")
 
         if item_id != page.meta.get('id', item_id):
-            raise Exception(f"Metadata ID doesn't match filename for {path}")
+            print(markdown)
+            print(page)
+            print(page.meta)
+            raise Exception(f"Metadata ID doesn't match filename for {path}: \n\tMetadata: {page.meta.get('id')}")
 
         page.meta['id'] = item_id
