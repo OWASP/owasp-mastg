9b21b7ffd36eb0a31facf613e26daf659cc59f97
Uncle Joe <1244005+sydseter@users.noreply.github.com>
2025-06-14 23:35:27 +0200

    Update demos/android/MASVS-NETWORK/MASTG-DEMO-0035/MastgTestWebView.kt
    Co-authored-by: Carlos Holguera <perezholguera@gmail.com>

--- demos/android/MASVS-NETWORK/MASTG-DEMO-0035/MastgTestWebView.kt
+++ demos/android/MASVS-NETWORK/MASTG-DEMO-0035/MastgTestWebView.kt
@@ -1,13 +1,13 @@
 package org.owasp.mastestapp
 
+import android.content.Context
 import android.net.http.SslError
 import android.util.Log
 import android.webkit.SslErrorHandler
 import android.webkit.WebView
 import android.webkit.WebViewClient
-import android.content.Context
 
-class MastgTestWebView (private val context: Context,) {
+class MastgTestWebView(private val context: Context) {
 
     fun mastgTest(webView: WebView) {
         webView.apply {
@@ -17,25 +17,24 @@ class MastgTestWebView (private val context: Context,) {
                     handler: SslErrorHandler,
                     error: SslError
                 ) {
-                    var message = "SSL Certificate error."
-                    when (error.getPrimaryError()) {
-                        SslError.SSL_UNTRUSTED -> message =
-                            "The certificate authority is not trusted."
+                    val message = getSslErrorMessage(error)
 
-                        SslError.SSL_EXPIRED -> message = "The certificate has expired."
-                        SslError.SSL_IDMISMATCH -> message = "The certificate Hostname mismatch."
-                        SslError.SSL_NOTYETVALID -> message = "The certificate is not yet valid."
-                        SslError.SSL_DATE_INVALID -> message =
-                            "The date of the certificate is invalid"
-                    }
-                    Log.w(null, "SSL errors onReceivedSslError: ".plus(message))
-                    Log.w(null, error.toString())
+                    Log.e("MastgTestWebView", "SSL errors onReceivedSslError: $message")
+                    Log.e("MastgTestWebView", error.toString())
 
                     handler.proceed()
                 }
-
             }
             loadUrl("https://tlsexpired.no")
         }
+    }
+
+    private fun getSslErrorMessage(error: SslError): String = when (error.primaryError) {
+        SslError.SSL_UNTRUSTED      -> "The certificate authority is not trusted."
+        SslError.SSL_EXPIRED        -> "The certificate has expired."
+        SslError.SSL_IDMISMATCH     -> "The certificate Hostname mismatch."
+        SslError.SSL_NOTYETVALID    -> "The certificate is not yet valid."
+        SslError.SSL_DATE_INVALID   -> "The date of the certificate is invalid."
+        else                        -> "SSL Certificate error."
     }
 }
