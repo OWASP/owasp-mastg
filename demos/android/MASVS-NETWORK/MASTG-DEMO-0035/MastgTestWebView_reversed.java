b37d09f2e28c2977629f18c6bc3f91de219bfd64
sydseter <johan.sydseter@admincontrol.com>
2025-06-14 23:54:29 +0200

    Generate reverse files and output

--- demos/android/MASVS-NETWORK/MASTG-DEMO-0035/MastgTestWebView_reversed.java
+++ demos/android/MASVS-NETWORK/MASTG-DEMO-0035/MastgTestWebView_reversed.java
@@ -10,7 +10,7 @@ import kotlin.Metadata;
 import kotlin.jvm.internal.Intrinsics;
 
 /* compiled from: MastgTestWebView.kt */
-@Metadata(d1 = {"\u0000\u001e\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0003\n\u0002\u0010\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\b\u0007\u0018\u00002\u00020\u0001B\u000f\u0012\u0006\u0010\u0002\u001a\u00020\u0003¢\u0006\u0004\b\u0004\u0010\u0005J\u000e\u0010\u0006\u001a\u00020\u00072\u0006\u0010\b\u001a\u00020\tR\u000e\u0010\u0002\u001a\u00020\u0003X\u0082\u0004¢\u0006\u0002\n\u0000¨\u0006\n"}, d2 = {"Lorg/owasp/mastestapp/MastgTestWebView;", "", "context", "Landroid/content/Context;", "<init>", "(Landroid/content/Context;)V", "mastgTest", "", "webView", "Landroid/webkit/WebView;", "app_debug"}, k = 1, mv = {2, 0, 0}, xi = 48)
+@Metadata(d1 = {"\u0000*\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0003\n\u0002\u0010\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0010\u000e\n\u0000\n\u0002\u0018\u0002\n\u0000\b\u0007\u0018\u00002\u00020\u0001B\u000f\u0012\u0006\u0010\u0002\u001a\u00020\u0003¢\u0006\u0004\b\u0004\u0010\u0005J\u000e\u0010\u0006\u001a\u00020\u00072\u0006\u0010\b\u001a\u00020\tJ\u0010\u0010\n\u001a\u00020\u000b2\u0006\u0010\f\u001a\u00020\rH\u0002R\u000e\u0010\u0002\u001a\u00020\u0003X\u0082\u0004¢\u0006\u0002\n\u0000¨\u0006\u000e"}, d2 = {"Lorg/owasp/mastestapp/MastgTestWebView;", "", "context", "Landroid/content/Context;", "<init>", "(Landroid/content/Context;)V", "mastgTest", "", "webView", "Landroid/webkit/WebView;", "getSslErrorMessage", "", "error", "Landroid/net/http/SslError;", "app_debug"}, k = 1, mv = {2, 0, 0}, xi = 48)
 /* loaded from: classes3.dex */
 public final class MastgTestWebView {
     public static final int $stable = 8;
@@ -29,29 +29,30 @@ public final class MastgTestWebView {
                 Intrinsics.checkNotNullParameter(view, "view");
                 Intrinsics.checkNotNullParameter(handler, "handler");
                 Intrinsics.checkNotNullParameter(error, "error");
-                String message = "SSL Certificate error.";
-                switch (error.getPrimaryError()) {
-                    case 0:
-                        message = "The certificate is not yet valid.";
-                        break;
-                    case 1:
-                        message = "The certificate has expired.";
-                        break;
-                    case 2:
-                        message = "The certificate Hostname mismatch.";
-                        break;
-                    case 3:
-                        message = "The certificate authority is not trusted.";
-                        break;
-                    case 4:
-                        message = "The date of the certificate is invalid";
-                        break;
-                }
-                Log.w((String) null, "SSL errors onReceivedSslError: " + message);
-                Log.w((String) null, error.toString());
+                String message = this.this$0.getSslErrorMessage(error);
+                Log.e("MastgTestWebView", "SSL errors onReceivedSslError: " + message);
+                Log.e("MastgTestWebView", error.toString());
                 handler.proceed();
             }
         });
         webView.loadUrl("https://tlsexpired.no");
+    }
+
+    /* JADX INFO: Access modifiers changed from: private */
+    public final String getSslErrorMessage(SslError error) {
+        switch (error.getPrimaryError()) {
+            case 0:
+                return "The certificate is not yet valid.";
+            case 1:
+                return "The certificate has expired.";
+            case 2:
+                return "The certificate Hostname mismatch.";
+            case 3:
+                return "The certificate authority is not trusted.";
+            case 4:
+                return "The date of the certificate is invalid.";
+            default:
+                return "SSL Certificate error.";
+        }
     }
 }
