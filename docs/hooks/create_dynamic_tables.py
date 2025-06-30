ade56f594aa07efa24912df80d94a5c05c126630
TheDauntless <info@dauntless.be>
2025-06-30 12:37:54 +0200

    troubleshoot

--- docs/hooks/create_dynamic_tables.py
+++ docs/hooks/create_dynamic_tables.py
@@ -81,7 +81,7 @@ def get_mastg_tests_dict():
 
     for file in glob.glob("docs/MASTG/tests/**/*.md", recursive=True):
         if "index.md" not in file:
-            print(file)
+            log.warn(file)
             with open(file, 'r') as f:
                 id = ""
                 content = f.read()
@@ -105,7 +105,7 @@ def get_mastg_tests_dict():
                         frontmatter['MASTG-TEST-ID'] = MASTG_TEST_ID
                         mastg_tests[id][platform].append(frontmatter)
                     else:
-                        print(f"No MASVS v2 coverage for: {frontmatter['title']} (was {frontmatter['masvs_v1_id']})")
+                        log.warn(f"No MASVS v2 coverage for: {frontmatter['title']} (was {frontmatter['masvs_v1_id']})")
                 except StopIteration:
                     continue
     return mastg_tests
