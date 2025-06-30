71c0e72d0f9a3f4d70c9706da3e0f5dffb78e247
TheDauntless <info@dauntless.be>
2025-06-30 13:13:50 +0200

    Update redirects

--- docs/hooks/add_redirects.py
+++ docs/hooks/add_redirects.py
@@ -1,16 +1,24 @@
 import os
 
 def on_config(config):
-    base_dir = "docs/MASTG/"
-    folders = ["tools", "apps", "techniques", "tests"]
+    folders = [
+        {"base": "docs/MASTG", "subfolders": ["tools", "apps", "techniques", "tests", "rules", "demos", "best-practices"]},
+        {"base": "docs/MASWE", "subfolders": [""]}
+    ]
 
     redirects_dict = {}
-    for folder in folders:
-        for root, _, files in os.walk(os.path.join(base_dir, folder)):
-            for file in files:
-                if file.endswith('.md'):
-                    relative_path = os.path.relpath(os.path.join(root, file), base_dir)
-                    redirects_dict[f"MASTG/{file}"] = f"MASTG/{relative_path}"
+
+    for folder_group in folders:
+        base_dir = folder_group["base"]
+        for subfolder in folder_group["subfolders"]:
+            folder_path = os.path.join(base_dir, subfolder)
+            for root, _, files in os.walk(folder_path):
+                for file in files:
+                    if "index.md" in file:
+                        continue
+                    if file.endswith('.md'):
+                        relative_path = os.path.relpath(os.path.join(root, file), "docs")
+                        redirects_dict[file] = relative_path.replace(os.sep, "/")
 
     # Ensure the 'redirects' plugin is present
     plugin = config['plugins'].get("redirects")
