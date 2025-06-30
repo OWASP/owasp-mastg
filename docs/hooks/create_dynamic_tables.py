7ef1869b767535e47161b418c813d1f29667aa78
TheDauntless <info@dauntless.be>
2025-06-30 12:39:26 +0200

    troubleshoot

--- docs/hooks/create_dynamic_tables.py
+++ docs/hooks/create_dynamic_tables.py
@@ -105,7 +105,8 @@ def get_mastg_tests_dict():
                         frontmatter['MASTG-TEST-ID'] = MASTG_TEST_ID
                         mastg_tests[id][platform].append(frontmatter)
                     else:
-                        log.warn(f"No MASVS v2 coverage for: {frontmatter['title']} (was {frontmatter['masvs_v1_id']})")
+                        log.warn(f"No MASVS v2 coverage for: {file}")
+                        # log.warn(f"No MASVS v2 coverage for: {frontmatter['title']} (was {frontmatter['masvs_v1_id']})")
                 except StopIteration:
                     continue
     return mastg_tests
