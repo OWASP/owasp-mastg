b491e4e21da2e7345dd57dd8320c3c9548f43df8
Carlos Holguera <perezholguera@gmail.com>
2025-07-15 09:48:43 +0200

    Update get_edit_url function to include edit_url_maswe for MASWE paths

--- docs/hooks/edit_button_uri.py
+++ docs/hooks/edit_button_uri.py
@@ -2,7 +2,7 @@ import logging
 
 log = logging.getLogger('mkdocs')
 
-def get_edit_url(src_path, edit_url_mastg, edit_url_masvs):
+def get_edit_url(src_path, edit_url_mastg, edit_url_masvs, edit_url_maswe):
     if src_path.startswith("MASVS"):
         edit_url = f"{edit_url_masvs}{src_path}"
         edit_url = edit_url.replace("master/MASVS/controls", "master/controls/")
@@ -16,8 +16,8 @@ def get_edit_url(src_path, edit_url_mastg, edit_url_masvs):
         if 'MASTG-TEST-02' in src_path:
             edit_url = edit_url.replace('/tests/', '/tests-beta/')
     elif src_path.startswith("MASWE"):
-        edit_url = f"{edit_url_mastg}{src_path}"
-        edit_url = edit_url.replace("master/MASWE/", "master/weaknesses/")
+        edit_url = f"{edit_url_maswe}{src_path}"
+        edit_url = edit_url.replace("main/MASWE/", "main/weaknesses/")
     elif src_path.startswith(("contributing", "donate")):
         edit_url = f"{edit_url_mastg}{src_path}"
         edit_url = edit_url.replace("master/", "master/docs/")
@@ -30,13 +30,14 @@ def on_pre_page(page, config, files):
     try:
         edit_url_mastg = "https://github.com/OWASP/mastg/edit/master/"
         edit_url_masvs = "https://github.com/OWASP/masvs/edit/master/"
+        edit_url_maswe = "https://github.com/OWASP/maswe/edit/main/"
     except KeyError:
         return page
 
     src_path = page.file.src_path
 
     if src_path.startswith(("MASTG", "MASVS", "MASWE", "contributing", "donate")):
-        edit_url = get_edit_url(src_path, edit_url_mastg, edit_url_masvs)
+        edit_url = get_edit_url(src_path, edit_url_mastg, edit_url_masvs, edit_url_maswe)
         if edit_url.endswith("/index.md"):
             page.edit_url = ""
         else:
